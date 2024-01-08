f34a6f05a34b16fbb2fd03eae31d0084f30c72d9
taman18 <Aarav15@>
2024-01-08 19:18:57 +0530

    5th commit

--- script.js
+++ script.js
@@ -236,7 +236,146 @@ let num;
     // console.log(num2);
     // let num3=num2.split();
     // console.log(num3);
+   
+    //08/01/2024
+    //slice method
+    // let num_array=[1,3,4,5,21,65];
+    // console.log(num_array);
+    // console.log(num_array.slice(1,4));   //3 4 5
+    // let num_array1=num_array.slice(1,4);
+    // console.log(num_array1);
+    // console.log(num_array);
+    // console.log(num_array.slice(1));  //3 4 5 21 65
+    // console.log(num_array);
     const fruits = ["Banana", "Orange", "Apple", "Kiwi"];
-    fruits.slice(1,4);
     console.log(fruits);
-    //hy this is my project
+    // fruits.splice(2,1,"mango");
+    fruits.splice(3,0,"mango");
+    // fruits.splice(1,2);
+    console.log(fruits);
+    // let new_fruits=fruits.splice(1,2);
+    // console.log(fruits.splice(1,2));
+    // console.log(fruits);
+    // const fruits = ["Banana", "Orange", "Apple", "Kiwi"];
+    // let fruits1=fruits.slice(-1,-3);
+    // console.log(fruits1);
+    // console.log(fruits);
+   
+
+    //reduce function
+    // let arr=[3,5,6,1,7];
+    // const output=arr.reduce(function(acc,curr){
+    //     if(acc<curr)
+    //     {
+    //         acc=curr;
+    //     }   
+    //     return acc;
+    // },arr[0])
+    // console.log(output);
+     
+
+
+    function shoppingSpree(arr) {
+        // your code heres
+        const Items_sum=arr.reduce(function(acc, curr)
+        {
+            return acc+curr.price;
+        },300)
+        return Items_sum;
+     }
+     
+     var wishlist = [
+         { title: "Tesla Model S", price: 90000 },
+         { title: "4 carat diamond ring", price: 45000 },
+         { title: "Fancy hacky Sack", price: 5 },
+         { title: "Gold fidgit spinner", price: 2000 },
+         { title: "A second Tesla Model S", price: 90000 }
+     ];
+     
+     console.log(shoppingSpree(wishlist));
+     
+     //A4 (task1)
+     let array = [12, 13, 14, 12, 13, 14, 12, 13, 14, 15, 99, 128];
+     //with loop
+    //  function Check_num(arr)
+    //  {
+    //     for(m of arr)
+    //     {
+    //         if(m===14) return true;
+    //     }
+    //     return false;
+    //  }
+    //  console.log("ans is->"+Check_num(array));
+    
+
+     //with inbuilt functions
+     function check80(arr)
+     {
+        console.log(arr.includes(14)); //return statement doubt
+     }
+     check80(array);
+     
+    //  A4(task2)
+    //  using for loop
+    // function check_80(arr)
+    // {
+    //     for(i of arr)
+    //     {
+    //         if(i<80) return false;
+    //     }
+    //     return true;
+    // }
+    // console.log(check_80(array));
+
+    //  using inbuilt function
+    // function check80(arr)
+    // {let count=0;
+    //     const find80=array.reduce(function(acc,curr){
+    //         if(curr>acc) count++;
+    // },80);
+    // if(count===array.length-1) return true;
+    // else return false;
+    // }
+    // console.log(check80(array));
+
+
+    function peopleWhoBelongToTheIlluminati(arr){
+        // your code here
+        let new_arr;
+        new_arr=arr.filter(arr=>arr.member===false);
+        return new_arr;
+      }
+      console.log(peopleWhoBelongToTheIlluminati([
+          { name: "Angelina Jolie", member: true },
+          { name: "Eric Jones", member: false },
+          { name: "Paris Hilton", member: true },
+          { name: "Kayne West", member: false },
+          { name: "Bob Ziroll", member: true }
+      ]));
+
+      
+    //   function occurence(arr)
+    //   {
+    //         arr.map((x)=>
+    //         {
+    //             let new_arr=
+    //         });
+    //   }
+    //   console.log(occurence(question));
+    let arr=[2,5,7,'a','b',2];
+    function find(x)
+    {
+        let count=0;
+       for(i of arr)
+       {
+         if(i===x) count++;
+       }
+       return count;
+}
+    console.log(arr.sort());
+    const occurence=arr.map(find);
+    console.log(occurence);
+    let new_occ=occurence;
+    new_occ.sort();
+    console.log(occurence);
+    let lastElement=occurence[occurence.length-1];
